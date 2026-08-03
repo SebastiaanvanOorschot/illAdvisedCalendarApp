c7884a69bb897e7cefcc8bbd29a5ee9abc0e0333
Sebastiaan <s.oorschot@zetacom.nl>
2026-08-03 16:07:12 +0200

    cc

---
+++ AgendaFrontend/src/constants/eventColors.ts
@@ -0,0 +1,14 @@
+// Color options matching Google Calendar style, shared across event/subscription color pickers.
+export const EVENT_COLOR_OPTIONS = [
+    '#000000', // Black (default)
+    '#FF0000', // Red
+    '#00FF00', // Green
+    '#0000FF', // Blue
+    '#FFFF00', // Yellow
+    '#FF00FF', // Magenta
+    '#00FFFF', // Cyan
+    '#FF8800', // Orange
+    '#8800FF', // Purple
+] as const;
+
+export const DEFAULT_EVENT_COLOR = '#000000';
